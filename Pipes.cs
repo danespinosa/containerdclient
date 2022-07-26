666c25ac861780eb082b6b3c2b1551f411baced8
Daniel Espinosa <luespino@microsoft.com>
2022-07-25 19:25:08 -0700

    latest tries

---
+++ Pipes.cs
@@ -0,0 +1,143 @@
+﻿// <copyright file="Pipes.cs" company="Microsoft">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// </copyright>
+
+
+    using System;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using Microsoft.Win32.SafeHandles;
+
+#nullable enable
+
+    /// <summary>
+    /// Interop structrues to create a Named Pipe.
+    /// </summary>
+    internal class Pipes
+    {
+        /// <summary>
+        /// File Overlapped flag.
+        /// </summary>
+        /// <see href="https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea"/>
+        internal const int FILE_FLAG_OVERLAPPED = 0x40000000;
+
+        /// <summary>
+        /// SQOS Security attribute.
+        /// </summary>
+        /// <see href="https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea"/>
+        internal const int SECURITY_SQOS_PRESENT = 0x100000;
+
+        /// <summary>
+        /// Anonymous security flag.
+        /// </summary>
+        /// <see href="https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea"/>
+        internal const int SECURITY_ANONYMOUS = 0;
+
+        /// <summary>
+        /// The pipe can read data.
+        /// </summary>
+        /// <see href="https://docs.microsoft.com/en-us/windows/win32/fileio/file-security-and-access-rights" />
+        /// <see href="https://docs.microsoft.com/en-us/windows/win32/fileio/file-access-rights-constants" />
+        internal const int FILE_READ_DATA = 1;
+
+        /// <summary>
+        /// The pipe can write attributes.
+        /// </summary>
+        // <see href="https://docs.microsoft.com/en-us/windows/win32/fileio/file-security-and-access-rights" />
+        // <see href="https://docs.microsoft.com/en-us/windows/win32/fileio/file-access-rights-constants" />
+        internal const int FILE_WRITE_ATTRIBUTES = 256;
+
+    internal const uint GENERIC_READ = 0x80000000;
+
+    internal const int GENERIC_WRITE = 0x40000000;
+
+    internal const uint INFINITE = 0xffffffff;
+
+    /// <summary>
+    /// Bool representation.
+    /// </summary>
+    /// <see href="https://github.com/dotnet/runtime/blob/a24364a09d9aea98b545f16689a53bafc6b18c14/src/libraries/Common/src/Interop/Windows/Interop.BOOL.cs"/>
+    internal enum BOOL : int
+        {
+            /// <summary>
+            /// False.
+            /// </summary>
+            FALSE = 0,
+
+            /// <summary>
+            /// True.
+            /// </summary>
+            TRUE = 1,
+        }
+
+    /// <summary>
+    /// Creates a Named Pipe file.
+    /// </summary>
+    /// <param name="lpFileName">The file name.</param>
+    /// <param name="dwDesiredAccess">The desired access.</param>
+    /// <param name="dwShareMode">The shared mode.</param>
+    /// <param name="secAttrs">The security attributes.</param>
+    /// <param name="dwCreationDisposition">The creation disposition.</param>
+    /// <param name="dwFlagsAndAttributes">The flags and attributes.</param>
+    /// <param name="hTemplateFile">The template file.</param>
+    /// <returns>The Safe Pipe handle.</returns>
+    /// <see href="https://docs.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea"/>
+    /// <see href="https://github.com/dotnet/runtime/blob/a24364a09d9aea98b545f16689a53bafc6b18c14/src/libraries/Common/src/Interop/Windows/Kernel32/Interop.CreateNamedPipeClient.cs"/>
+    [DllImport("kernel32.dll", EntryPoint = "CreateFileW", CharSet = CharSet.Unicode, SetLastError = true, BestFitMapping = false)]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+        internal static extern IntPtr CreateNamedPipeClient(
+          string? lpFileName,
+          uint dwDesiredAccess,
+          FileShare dwShareMode,
+          ref SECURITY_ATTRIBUTES secAttrs,
+          FileMode dwCreationDisposition,
+          uint dwFlagsAndAttributes,
+          IntPtr hTemplateFile);
+
+    //[DllImport("kernel32.dll", SetLastError = true)]
+    //internal static extern bool WriteFile(IntPtr hFile, byte[] lpBuffer, uint nNumberOfBytesToWrite, out uint lpNumberOfBytesWritten, [In] ref System.Threading.NativeOverlapped lpOverlapped);
+
+    [DllImport("kernel32.dll", BestFitMapping = false, CharSet = CharSet.Unicode, SetLastError = true)]
+    internal static extern bool WriteFile(IntPtr hFile, byte[] lpBuffer, uint nNumberOfBytesToWrite, out uint lpNumberOfBytesWritten, [In] ref System.Threading.NativeOverlapped lpOverlapped);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    internal static extern bool GetOverlappedResult(IntPtr hFile,
+   [In] ref System.Threading.NativeOverlapped lpOverlapped,
+   out uint lpNumberOfBytesTransferred, bool bWait);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    internal static extern IntPtr CreateIoCompletionPort(IntPtr FileHandle,
+   IntPtr ExistingCompletionPort, UIntPtr CompletionKey,
+   uint NumberOfConcurrentThreads);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    internal static extern bool SetFileCompletionNotificationModes(IntPtr fileHandle, uint flags);
+
+    [DllImport("kernel32.dll")]
+   internal  static extern bool GetQueuedCompletionStatus(IntPtr CompletionPort, out uint
+   lpNumberOfBytes, out UIntPtr lpCompletionKey, out IntPtr lpOverlapped, uint dwMilliseconds);
+
+    /// <summary>
+    /// File security attributes.
+    /// </summary>
+    /// <see href="https://github.com/dotnet/runtime/blob/a24364a09d9aea98b545f16689a53bafc6b18c14/src/libraries/Common/src/Interop/Windows/Kernel32/Interop.SECURITY_ATTRIBUTES.cs"/>
+    [StructLayout(LayoutKind.Sequential)]
+        internal struct SECURITY_ATTRIBUTES
+        {
+            /// <summary>
+            /// Length.
+            /// </summary>
+            internal uint nLength;
+
+            /// <summary>
+            /// Descriptor.
+            /// </summary>
+            internal IntPtr lpSecurityDescriptor;
+
+            /// <summary>
+            /// Inherit handle.
+            /// </summary>
+            internal BOOL bInheritHandle;
+        }
+    }
+#nullable disable
--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿using Containerd.Services.Containers.V1;
 using Grpc.Core;
 using Grpc.Net.Client;
+using Microsoft.Win32.SafeHandles;
+using System;
 using System.IO.Pipes;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
+using System.Text;
+using static Pipes;
 
 const string sockPath = "/run/containerd/containerd.sock";
-const string pipeName = "containerd-containerd";
+const string pipeName = @"\\.\pipe\containerd-containerd";
 bool isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 Console.WriteLine($"Check pipe: {File.Exists(@"\\.\pipe\containerd-containerd")}");
 IConnectionFactory connectionFactory = isWindows ? new NamedPipeConnectionFactory(pipeName) : new UnixDomainSocketConnectionFactory(sockPath);
@@ -39,35 +43,169 @@ else
 public sealed class NamedPipeConnectionFactory : IConnectionFactory
 {
     private readonly string _pipeName;
-    private readonly NamedPipeClientStream _pipe;
+    private IntPtr handle;
+    private NamedPipeClientStream _pipe;
+    private IntPtr completionHandle;
 
     public NamedPipeConnectionFactory(string pipeName) {
         _pipeName = pipeName;
-        _pipe = new NamedPipeClientStream(".", _pipeName,
-                direction: PipeDirection.InOut,
-               options: PipeOptions.Asynchronous,
-               impersonationLevel: TokenImpersonationLevel.Anonymous,
-               inheritability: System.IO.HandleInheritability.None);
+       
+        //this._pipe = new NamedPipeClientStream(PipeDirection.InOut, isAsync: true, isConnected: true, safePipeHandle: this.handle);
     }
+
+    public async ValueTask<Stream> ConnectAsync2(SocketsHttpConnectionContext socketsHttpConnectionContext, CancellationToken cancellationToken)
+    {
+        var buffer = new byte[24];
+        int numWrite = Encoding.ASCII.GetBytes("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n", buffer);
+        var client = new NamedPipeClientStream(".",
+            "containerd-containerd",
+            PipeDirection.InOut,
+            PipeOptions.Asynchronous,
+            TokenImpersonationLevel.Anonymous);
+
+        await client.ConnectAsync();
+        await client.WriteAsync(buffer.AsMemory(0, numWrite));
+        return client;
+    }
+
+    public async ValueTask<Stream> ConnectAsync3(SocketsHttpConnectionContext socketsHttpConnectionContext, CancellationToken cancellationToken) 
+    {
+        // Write preface
+        int pipeFlags = SECURITY_SQOS_PRESENT | SECURITY_ANONYMOUS | FILE_FLAG_OVERLAPPED;
+        //int fileAccess = unchecked((int)(GENERIC_READ | GENERIC_WRITE));
+        uint fileAccess = GENERIC_READ | GENERIC_WRITE;
+        //int fileAccess = FILE_READ_DATA | FILE_WRITE_ATTRIBUTES;
+        var secAttributes = default(SECURITY_ATTRIBUTES);
+        this.handle = CreateNamedPipeClient(
+            $"\\\\.\\pipe\\containerd-containerd",// _pipeName,
+            fileAccess,           // read access that allows to set ReadMode to message on lines 114 & 172
+            0,                  // sharing: none
+            ref secAttributes,           // security attributes
+            FileMode.Open,      // open existing
+            SECURITY_SQOS_PRESENT | SECURITY_ANONYMOUS | FILE_FLAG_OVERLAPPED,         // impersonation flags
+            IntPtr.Zero);  // template file: null
+        int error = Marshal.GetLastWin32Error();
+
+        
+        var handle2 = CreateNamedPipeClient(
+            $"\\\\.\\pipe\\containerd-containerd",// _pipeName,
+            fileAccess,           // read access that allows to set ReadMode to message on lines 114 & 172
+            0,                  // sharing: none
+            ref secAttributes,           // security attributes
+            FileMode.Open,      // open existing
+            SECURITY_SQOS_PRESENT | SECURITY_ANONYMOUS | FILE_FLAG_OVERLAPPED,         // impersonation flags
+            IntPtr.Zero);  // template file: null
+        error = Marshal.GetLastWin32Error();
+        var buffer = new byte[24];
+        int numWrite = Encoding.ASCII.GetBytes("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n", buffer);
+        NativeOverlapped nativeOverlapped = new NativeOverlapped();
+        ManualResetEvent eventWaitHandle = new ManualResetEvent(false);
+        var writtenBytes = (uint)numWrite;
+
+        _pipe = new NamedPipeClientStream(PipeDirection.InOut, isAsync: true, isConnected: true, safePipeHandle: new SafePipeHandle(handle2, ownsHandle: true));
+        var buffer2 = new byte[24];
+        //await _pipe.ConnectAsync();
+        await _pipe.WriteAsync(buffer.AsMemory(0, numWrite));
+        return _pipe;
+    }
+
+
+
     public async ValueTask<Stream> ConnectAsync(SocketsHttpConnectionContext socketsHttpConnectionContext, CancellationToken cancellationToken)
     {
      
         try
         {
-            await _pipe.ConnectAsync(timeout: 1000, cancellationToken);
+            // await _pipe.ConnectAsync(timeout: 1000, cancellationToken);
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                _pipe.ReadMode = PipeTransmissionMode.Byte;
+                //_pipe.ReadMode = PipeTransmissionMode.Byte;
             }
+
+            // Write preface
+            int pipeFlags = FILE_FLAG_OVERLAPPED | SECURITY_SQOS_PRESENT | SECURITY_ANONYMOUS;
+            //int fileAccess = unchecked((int)(GENERIC_READ | GENERIC_WRITE));
+            uint fileAccess = GENERIC_READ | GENERIC_WRITE;
+            //int fileAccess = FILE_READ_DATA | FILE_WRITE_ATTRIBUTES;
+            var secAttributes = default(SECURITY_ATTRIBUTES);
+            this.handle = CreateNamedPipeClient(
+                $"\\\\.\\pipe\\containerd-containerd",// _pipeName,
+                fileAccess,           // read access that allows to set ReadMode to message on lines 114 & 172
+                0,                  // sharing: none
+                ref secAttributes,           // security attributes
+                FileMode.Open,      // open existing
+                FILE_FLAG_OVERLAPPED | SECURITY_SQOS_PRESENT | SECURITY_ANONYMOUS,         // impersonation flags
+                IntPtr.Zero);  // template file: null
+            int error = Marshal.GetLastWin32Error();
+            
+            //StartCompletionRoutine();
+            //var fileCompletionPort = CreateIoCompletionPort(this.handle, this.completionHandle, UIntPtr.Zero, 4294967295);
+            //error = Marshal.GetLastWin32Error();
+            //bool notification = SetFileCompletionNotificationModes(this.handle, 1 | 2);
+
+            var buffer = new byte[24];
+            int numWrite = Encoding.ASCII.GetBytes("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n", buffer);
+            NativeOverlapped nativeOverlapped = new NativeOverlapped();
+            ManualResetEvent eventWaitHandle = new ManualResetEvent(false);
+            nativeOverlapped.EventHandle = eventWaitHandle.SafeWaitHandle.DangerousGetHandle();
+            //IntPtr intPtr = this.handle.DangerousGetHandle();
+            var writtenBytes = (uint)numWrite;
+
+            //_pipe = new NamedPipeClientStream(PipeDirection.InOut, isAsync: true, isConnected: true, safePipeHandle: this.handle);
+            //_pipe = new NamedPipeClientStream(PipeDirection.InOut, isAsync: true, isConnected: true, safePipeHandle: new SafePipeHandle(handle, ownsHandle:false));
+            //await _pipe.WriteAsync(buffer, 0, numWrite);
+            //var buffer2 = new byte[64];
+            //int numWrite2 = Encoding.ASCII.GetBytes("\x00\x00\x00\x04\x00\x00\x00\x00\x00", buffer2);
+            //await _pipe.WriteAsync(buffer2, 0, numWrite2);
+
+            //await Task.WhenAny(preface, settings);
+
+            var write = WriteFile(this.handle, buffer, writtenBytes, out uint bytesWritten, ref nativeOverlapped);
+            error = Marshal.GetLastWin32Error();
+            uint bytesWritten6;
+            if (error == 997)
+                GetOverlappedResult(handle, ref nativeOverlapped, out bytesWritten6, true);
+            eventWaitHandle.WaitOne();
+
+
+
+            await Task.Delay(8000);
+            //write = WriteFile(handle, buffer, writtenBytes, out uint bytesWritten2, ref nativeOverlapped);
+            //error = Marshal.GetLastWin32Error();
+            //_pipe.Write(buffer, 0, numWrite);
+            // Write Settings
+            //numWrite = Encoding.ASCII.GetBytes(String.Empty, buffer);
+            //WriteFile(handle, buffer, (uint)numWrite, out uint bytesWritten3, ref nativeOverlapped);
 
             Console.WriteLine("connected");
         }
         catch
         {
-            await _pipe.DisposeAsync();
+            //await _pipe.DisposeAsync();
             throw;
         }
+        _pipe = new NamedPipeClientStream(PipeDirection.InOut, isAsync: true, isConnected: true, safePipeHandle: new SafePipeHandle(handle, ownsHandle:true));
         return _pipe;
+    }
+
+    private void StartCompletionRoutine()
+    {
+        this.completionHandle = CreateIoCompletionPort(IntPtr.MaxValue, IntPtr.Zero, UIntPtr.Zero, 4294967295);
+        _ = Task.Run(() =>
+        {
+            while (true)
+            {
+                UIntPtr key;
+                IntPtr overlapped;
+                bool result = GetQueuedCompletionStatus(completionHandle, out uint lpNumberOfBytes, out key, out overlapped, INFINITE);
+                if (result) 
+                {
+                    int error = Marshal.GetLastWin32Error();
+                    Console.WriteLine($"Error in queue {error}");
+                }
+                Console.WriteLine("Got status");
+            }
+        }).ConfigureAwait(false);
     }
 }
 
